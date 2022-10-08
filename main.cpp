b6faa21f4fa8e82b41d0be8873f72c01bca87cdb
Sam <samirssr24@gmail.com>
2022-10-08 00:51:13 -0500

    Funcionawii

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include "My_utilities.h"
 
 using namespace std;
 
 int main()
 {
     int a = 2;
+    int *arr;
     cout << a;
+
+    array_creator(arr, 3);
+    array_filler(arr, 3);
+    array_printer(arr, 3);
+    delete[] arr;
 
     return 0;
 }
