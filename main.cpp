89b3cbe91a8890187107d4fe7482b0b7fd3acc90
Sam <samirssr24@gmail.com>
2022-10-08 21:50:18 -0500

    Funcion paso

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ double probabilidad(int min = 0, int max = 100, double decimals = 100);
 
 int main()
 {
+    const int autos = 8;
+    string *nombres;
     double *distancia;
     double *velocidad;
 
@@ -19,18 +21,39 @@ double probabilidad(int min, int max, double decimals)
     return (min + rand() % (max + 1 - min)) / decimals;
 }
 
-void paso(double p, double distancia)
+void paso(double p, double *&distancia, int idx)
 {
+    if (p > 0.8)
+    {
+        distancia[idx] += 83;
+    }
+    else if (p > 0.6)
+    {
+        distancia[idx] += 50;
+    }
+    else if (p > 0.4)
+    {
+        distancia[idx] += 30;
+    }
+    else if (p > 0.2)
+    {
+        distancia[idx];
+    }
+    else
+    {
+        distancia[idx] -= 10;
+    }
 }
 
-void inicializador(double *&distancia, double *&velocidad)
+void inicializador(double *&distancia, double *&velocidad, string *&nombres, int n)
 {
-    array_creator(distancia, 8);
-    array_creator(velocidad, 8);
+    array_creator(distancia, n);
+    array_creator(velocidad, n);
 }
 
-void finalizador(double *&distancia, double *&velocidad)
+void finalizador(double *&distancia, double *&velocidad, string *&nombres)
 {
     delete[] distancia;
     delete[] velocidad;
+    delete[] nombres;
 }
