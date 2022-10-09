9dc032af199e5a310d12ef145479efbc0a5e2613
Sam <samirssr24@gmail.com>
2022-10-08 21:41:00 -0500

    Funcion de inicializar y Funcion de finalizar

--- main.cpp
+++ main.cpp
@@ -8,10 +8,29 @@ double probabilidad(int min = 0, int max = 100, double decimals = 100);
 
 int main()
 {
+    double *distancia;
+    double *velocidad;
+
     return 0;
 }
 
 double probabilidad(int min, int max, double decimals)
 {
     return (min + rand() % (max + 1 - min)) / decimals;
+}
+
+void paso(double p, double distancia)
+{
+}
+
+void inicializador(double *&distancia, double *&velocidad)
+{
+    array_creator(distancia, 8);
+    array_creator(velocidad, 8);
+}
+
+void finalizador(double *&distancia, double *&velocidad)
+{
+    delete[] distancia;
+    delete[] velocidad;
 }
